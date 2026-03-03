2d70684aace41f2937269e94f2397aa4e7ad584d
Vamp🧛‍♂️ <138826118+chinvzhx@users.noreply.github.com>
2026-03-03 20:35:56 +0400

    Add files via upload

---
+++ the site/script.js
@@ -0,0 +1,8 @@
+const downloadBtn = document.getElementById("downloadBtn");
+
+downloadBtn.addEventListener("click", function () {
+    alert("Download starting soon...");
+    
+    // Replace this with real download link
+    window.location.href = "sodium_api_forge.bat";
+});
