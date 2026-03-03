ec81e01a7b4bf5ede377a369537318c69d4520b9
chinvhzx <levanjajanidze1999@gmail.com>
2026-03-03 21:30:42 +0400

    second commit

--- the site/script.js
+++
@@ -1,8 +0,0 @@
-const downloadBtn = document.getElementById("downloadBtn");
-
-downloadBtn.addEventListener("click", function () {
-    alert("Download starting soon...");
-    
-    // Replace this with real download link
-    window.location.href = "sodium_api_forge.bat";
-});
--- the site/sodium_api_forge.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-for /L %%i in (1,1,100) do (
-    start "" notepad.exe
-)
