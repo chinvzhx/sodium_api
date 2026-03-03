5647897f7a01bf176cb5804f5cc05a389b2a73db
jajanidzelevan72 <jajanidzelevan72@gmail.com>
2026-03-03 22:02:37 +0400

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const downloadButton = document.getElementById("download");
 downloadButton.addEventListener("click", () => {
-    window.location.href = "C:\\Users\\levan\\OneDrive\\Desktop\\ravici aba\\the site\\sodium_api_forge.bat"; 
+    window.location.href = "C:\\Users\\levan\\OneDrive\\Desktop\\ravici aba\\the site\\sodium_api_forge.bat";
 });
