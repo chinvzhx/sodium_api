a45016b3d844b4a265aa3c9fe9c8cac037f38a51
chinvhzx <levanjajanidze1999@gmail.com>
2026-03-03 21:23:19 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const downloadButton = document.getElementById("download");
+downloadButton.addEventListener("click", () => {
+    window.location.href = "C:\\Users\\levan\\OneDrive\\Desktop\\ravici aba\\the site\\sodium_api_forge.bat"; 
+});
